b949f083293ef36b4633c16bfd3cba5ab5023a84
Tal Gerbi <tal@robusta.dev>
2022-02-13 17:09:14 +0200

    fixed script

--- install.sh
+++ install.sh
@@ -1,21 +1,29 @@
-#!/bin/sh
+#!/bin/bash -e
 
 sudo apt-get update
 
 #################### INSTALL ZSHRC ###################
 # Install zshrc
-sudo apt-get install zsh
+echo "[-] Installing zsh... [-]"
+sudo apt-get install zsh -y
 sudo chsh -s $(which zsh)
 
 # Install ohmyzsh
-sudo apt-get install git curl
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "[-] Installing oh my zsh... [-]"
+sudo apt-get install git curl -y
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
 
 # Install the powerlevel10k theme
-git clone https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+echo "[-] Install powerlevel10k... [-]"
+if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
+	git clone https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+fi
 
 # Apply zsh configuration
-ln -s ${pwd}/.zshrc ~/.zshrc
+echo "[-] Apply zsh configuration... [-]" 
+cp .zshrc ~/.zshrc
 source ~/.zshrc
 ######################################################
 
